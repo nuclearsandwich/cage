bc203286f832d9586888655ff95bff8064c9814b
Steven! Ragnarök <steven@nuclearsandwich.com>
2012-01-26 11:40:39 -0800

    Save and print last_response.

--- lib/cage/console.rb
+++ lib/cage/console.rb
@@ -2,14 +2,17 @@ module Cage
   class Console
     CONNECTION_VARIABLES = [:scheme, :domain, :prefix, :headers]
 
-    attr_reader :connection, *CONNECTION_VARIABLES
+    attr_reader :connection, :last_response, *CONNECTION_VARIABLES
 
     def initialize
+      @scheme = "http"
+      @domain = "duckduckgo.com"
+      @prefix = "?q="
       reinitialize_connection
     end
 
     def reinitialize_connection
-      @connection = Faraday::Connection.new "#{domain}/#{prefix}"
+      @connection = Faraday::Connection.new "#{scheme}://#{domain}/#{prefix}"
     end
 
     def method_missing sym, *args, &block
@@ -20,10 +23,13 @@ module Cage
       end
     end
 
+    def print_response
+      ap [ last_response.status, last_response.headers, last_response.body ]
     end
 
     def http method, *args, &block
-      connection.send method, *args, &block
+      @last_response = connection.send method, *args, &block
+      print_response
     end
 
     def set convar, value
